7f419ed1cf068c5466a1ea9aef27f75e49d5c2b3
Hilal <hilal_yazbeck@hotmail.com>
2024-03-15 13:23:51 +0300

    Add project files.

---
+++ Services/ChatService.cs
@@ -0,0 +1,33 @@
+﻿using Azure;
+using Azure.AI.OpenAI;
+
+namespace BlazorChatbot.Services;
+
+public class ChatService : IChatService
+{
+    public async Task<ChatCompletions> ChatWithOpenAI(string userMessage)
+    {
+
+        var _client = new OpenAIClient(
+          new Uri("https://seesharpopenai.openai.azure.com/"),
+          new AzureKeyCredential("[YOUR KEY HERE]"));
+
+        Console.WriteLine(userMessage);
+        Response<ChatCompletions> responseWithoutStream = await _client.GetChatCompletionsAsync(
+            "seesharp",
+            new ChatCompletionsOptions()
+            {
+                Messages =
+                            {
+                    new ChatMessage(ChatRole.User, userMessage)
+                            },
+                Temperature = (float)0.7,
+                MaxTokens = 800,
+                NucleusSamplingFactor = (float)0.95,
+                FrequencyPenalty = 0,
+                PresencePenalty = 0
+            });
+
+        return responseWithoutStream.Value;
+    }
+}
---
+++ Services/IChatService.cs
@@ -0,0 +1,8 @@
+﻿using Azure.AI.OpenAI;
+
+namespace BlazorChatbot.Services;
+
+public interface IChatService
+{
+    Task<ChatCompletions> ChatWithOpenAI(string userMessage);
+}
